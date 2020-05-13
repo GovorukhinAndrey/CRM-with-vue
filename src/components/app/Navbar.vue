30543fa2263c4faa8848e848aaa9b029d3ef2403
Andrey Govorukhin <andr.govorukhin@gmail.com>
2020-05-13 14:41:51 +0100

    Вывод даты и времени в навбаре

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -5,7 +5,7 @@
         <a href="#" @click.prevent="$emit('click')">
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">{{ date | date('datetime') }}</span>
       </div>
 
       <ul class="right hide-on-small-and-down">
@@ -36,11 +36,25 @@
 <script>
 export default {
   name: 'Navbar',
+  data: () => ({
+    date: new Date(),
+    interval: null,
+    dropdown: null,
+  }),
   mounted() {
     // eslint-disable-next-line no-undef
     this.dropdown = M.Dropdown.init(this.$refs.dropdown, {
       constrainWidth: false,
     });
+    this.interval = setInterval(() => {
+      this.date = new Date();
+    }, 1000);
+  },
+  beforeDestroy() {
+    if (this.dropdown && this.dropdown.destroy) {
+      this.dropdown.destroy();
+    }
+    clearInterval(this.interval);
   },
   methods: {
     logout() {
