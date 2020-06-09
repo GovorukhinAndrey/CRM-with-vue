dbaf31b74f858a35eb74ef9a8666837419a0b55c
Andrey Govorukhin <andr.govorukhin@gmail.com>
2020-06-09 16:48:48 +0100

    добавил vuelidate

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
-import './registerServiceWorker';
+import Vuelidate from 'vuelidate';
 import router from './router';
 import store from './store';
 import dateFilter from '@/filters/date.filter';
+import './registerServiceWorker';
 import 'materialize-css/dist/js/materialize.min.js';
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuelidate);
 Vue.filter('date', dateFilter);
 
 new Vue({
