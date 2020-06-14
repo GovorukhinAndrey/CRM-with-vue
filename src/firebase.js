df4ef8efc3d327aa70201d395266160b071af34a
Andrey Govorukhin <andr.govorukhin@gmail.com>
2020-06-14 12:38:52 +0100

    Подключил firebase

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDlDqJE05WvGbQmKS77EeLIIe3pBii9SAU',
+  authDomain: 'crm-with-vue.firebaseapp.com',
+  databaseURL: 'https://crm-with-vue.firebaseio.com',
+  projectId: 'crm-with-vue',
+  storageBucket: 'crm-with-vue.appspot.com',
+  messagingSenderId: '50840411605',
+  appId: '1:50840411605:web:5092258e0ef2865e69715c',
+  measurementId: 'G-PSGRMPEZYX',
+};
+
+// // Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const firebaseDatabase = firebaseApp.database();
+export const firebaseAuth = firebaseApp.auth();
+
+export default firebaseApp;
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import dateFilter from '@/filters/date.filter';
 import messagePlugin from '@/utils/message.plugin';
 import './registerServiceWorker';
 import 'materialize-css/dist/js/materialize.min.js';
+import { firebaseAuth } from '@/firebase';
 
 Vue.config.productionTip = false;
 
@@ -14,8 +15,14 @@ Vue.use(messagePlugin);
 Vue.use(Vuelidate);
 Vue.filter('date', dateFilter);
 
-new Vue({
-  router,
-  store,
-  render: h => h(App),
-}).$mount('#app');
+let app;
+
+firebaseAuth.onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App),
+    }).$mount('#app');
+  }
+});
