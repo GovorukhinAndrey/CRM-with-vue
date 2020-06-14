622ab58fec20ebfa235e1174f22a22364ba9e826
Andrey Govorukhin <andr.govorukhin@gmail.com>
2020-06-14 18:20:27 +0100

    Добавил фильтр currency

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Vuelidate from 'vuelidate';
 import router from './router';
 import store from './store';
 import dateFilter from '@/filters/date.filter';
+import currencyFilter from '@/filters/currency.filter';
 import messagePlugin from '@/utils/message.plugin';
 import './registerServiceWorker';
 import 'materialize-css/dist/js/materialize.min.js';
@@ -17,6 +18,7 @@ Vue.config.productionTip = false;
 Vue.use(messagePlugin);
 Vue.use(Vuelidate);
 Vue.filter('date', dateFilter);
+Vue.filter('currency', currencyFilter);
 Vue.component('Loader', Loader);
 
 let app;
